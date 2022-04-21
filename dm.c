c460d4accbe7d957263d445915ecb940ab621964
ACHERIR M. AREZKI <39704385+MohandAcherir@users.noreply.github.com>
2022-04-21 03:38:37 -0400

    Add files via upload

--- dm.c
+++ dm.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
+#include <string.h>
 #include "dm.h"
 
 void SnM(mpz_t a, mpz_t n, mpz_t h) {
-	mpz_t i, hi, tmp, r;
-	mpz_init(i); 
-	mpz_init(hi); 
-	mpz_init(tmp);
+	mpz_t r;
+
 	mpz_init(r);
 
 	mpz_set(r, a);
 
-	mpz_cdiv_q(h, h, deux);
-	mpz_sub(tmp, h, un);
-	mpz_set(i, tmp);
+	char* str = mpz_get_str(NULL, 2, h);
 
-	while(mpz_cmp(i, zero) >= 0) {
+	for(int t = strlen(str)-2 ; t >= 0 ; t--) {
 		mpz_mul(r, r, r);
 		mpz_mod(r, r, n);
 
-		mpz_mod(tmp, h, deux);
-		if((mpz_cmp(h, zero) != 0) && (mpz_cmp(tmp, zero) == 0)) {
-			mpz_cdiv_q(h, h, deux);
+		if(str[t] == '1') {
 			mpz_mul(r, r, a);
 			mpz_mod(r, r, n);
 		}
-		mpz_sub(i, i, un);
 	}
 	mpz_set(snm, r);
 
-	mpz_clear(i);
-	mpz_clear(hi);
-	mpz_clear(tmp);
 	mpz_clear(r);
+	free(str);
 	
 	return;
 }
@@ -62,24 +54,25 @@ char Miller_Rabin(mpz_t n, int k) {
 	
 	int s = 0;
 
-	while(mpz_cmp(zero, r) == 0) {
+	while((mpz_cmp(zero, t) != 0) && (mpz_cmp(zero, r) == 0)) {
 		s++;
-		mpz_cdiv_q(t, t, deux);
+		mpz_tdiv_q(t, t, deux);
 		mpz_mod(r, t, deux);
 	}
 
 	int i = 1;
 
+	//printf("%d, ", s);
+	//mpz_out_str(stdout, 10, t);
+
+
 	while( i <= k ) {
 here:
 		mpz_set(tmp, n);
-		mpz_sub(tmp, tmp, un);
+		mpz_sub(tmp, tmp, deux);
+		mpz_sub(tmp, tmp, deux);
 		mpz_urandomm(a, state, tmp);
-		mpz_add(a, a, un);
-
-
-		mpz_mod(a, a, minus);
-		mpz_add(a, a, un);
+		mpz_add(a, a, deux);
 
 		/*mpz_set(a1, a);
 	 	while(mpz_cmp(un, t) < 0) {
@@ -87,40 +80,44 @@ here:
 	 		mpz_sub(t, t, un);
 	 	}*/
 		SnM(a, n, t);
-	 	mpz_set(a, snm);
-
-	 	mpz_mod(y, a, n);
+	 	mpz_set(y, snm);
 
 	 	while(mpz_cmp(y, n) >= 0) {
 	 		mpz_mod(y, y, n);	
 	 	}
-	 	mpz_mod(tmp, y, n);
 
-	 	if( (mpz_cmp(tmp, un) != 0) && ((mpz_cmp(tmp, minus) != 0) || (mpz_cmp(tmp, mun) != 0)) ) {
-	 		for(int j = 1 ; j < s ; j++) {
-	 			mpz_mul(y, y, y);
-	 			mpz_mod(y, y, n);
-	 			mpz_mod(tmp, y, n);
+	 	mpz_set(tmp, y);
 
-	 			if(mpz_cmp(tmp, un) == 0) {
-	 				return 'c';
+	 	if( (mpz_cmp(tmp, un) == 0) || (mpz_cmp(tmp, minus) == 0) ) {
+	 		i++;
+
+	 		if(i <= k) {
+	 			goto here;
+	 		}
+	 		else {
+	 			goto there;
+	 		}
+	 	}	
+	 	
+	 	for(int j = 1 ; j < s ; j++) {
+	 		mpz_mul(y, y, y);
+	 		mpz_mod(y, y, n);
+
+	 		if(mpz_cmp(y, minus) == 0) {
+	 			i++;
+
+	 			if(i <= k) {
+	 				goto here;
 	 			}
-	 			if((mpz_cmp(tmp, minus) == 0) || (mpz_cmp(tmp, mun) == 0)) {
-	 				i++;
-
-	 				if(i <= k) {
-	 					goto here;
-	 				}
-	 				else {
-	 					goto there;
-	 				}
+	 			else {
+	 				goto there;
 	 			}
 	 		}
-	 	
-	 		return 'c';
 	 	}
-	 	i++;
+	  	return 'c';
+		i++;
 	}
+	 	
 
 there:
 	mpz_clear(r);
@@ -137,13 +134,15 @@ there:
 
 
 int main() {
-	mpz_t n, t;
+	mpz_t n, t, neuf, ss;
 
 	mpz_init_set_str(deux, "2", 10);
 	mpz_init_set_str(un, "1", 10);
 	mpz_init_set_str(zero, "0", 10);
 	mpz_init_set_str(n, "3", 10);
-	mpz_init_set_str(t, "3", 10);
+	mpz_init_set_str(t, "7", 10);
+	mpz_init_set_str(neuf, "3", 10);
+	mpz_init_set_str(ss, "77", 10);
 	mpz_init(snm);
 
 	char str[4];
@@ -158,16 +157,18 @@ int main() {
 		else {
 			sprintf(str, "%d", i);
 			mpz_init_set_str(n, str, 10);
-			printf("%d: %c\n", i, Miller_Rabin(n, i/4));
+			printf("%d: %c\n", i, Miller_Rabin(n, i));
 		}	
 		str[0] = '\0';
 	}
-	/*SnM(deux, n, t);
+	//SnM(neuf, ss, t);
 
-	mpz_out_str(stdout, 10, snm);*/
+	//mpz_out_str(stdout, 10, snm);
 
 	mpz_clear(n);
 	mpz_clear(t);
+	mpz_clear(neuf);
+	mpz_clear(ss);
 	mpz_clear(snm);
 	mpz_clear(zero);
 	mpz_clear(un);
